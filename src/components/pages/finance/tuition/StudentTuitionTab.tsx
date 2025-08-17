587b63341902f47819059f9317df6a4fc63d789e
ojan <milyasa2468@gmail.com>
2025-08-17 16:39:01 +0700

    fix:fixing minor

--- src/components/pages/finance/tuition/StudentTuitionTab.tsx
+++ src/components/pages/finance/tuition/StudentTuitionTab.tsx
@@ -10,8 +10,8 @@ import LoadingSkeletonTable from "@/components/shared/LoadingSkeletonTable";
 
 const StudentTuitionTab = () => {
   const { data, isPending, isError } = useGetTuitionQuery({
-    classId: "06dc95f4-0f5a-4cf4-aba9-d6865d6e55e3",
-    month: "2025-01",
+    classId: "6507a04f-d5c1-479e-a6b4-55602b09351a",
+    month: "2025-08",
   });
   const tuitionPayments = paymentData.filter(
     (item) => item.jenisPembayaran === "SPP"
@@ -34,18 +34,18 @@ const StudentTuitionTab = () => {
     }, 0);
 
   return (
-    <div className="w-full space-y-4">
+    <div className='w-full space-y-4'>
       <StudentTuitionFilters />
       {isPending ? (
         <LoadingSkeletonTable />
       ) : isError ? (
-        <div className="flex w-full h-full items-center  text-red-500 font-semibold">
-          <span className="text-center">
+        <div className='flex w-full h-full items-center  text-red-500 font-semibold'>
+          <span className='text-center'>
             Terjadi kesalahan saat mengambil data SPP.
           </span>
         </div>
       ) : (
-      <StudentTuitionTable data={data?.data} />
+        <StudentTuitionTable data={data?.data} />
       )}
     </div>
   );
